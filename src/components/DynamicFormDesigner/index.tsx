3724a6caaa3786f714ad00c6dd805a650e1f5c83
izsl <iamppz@hotmail.com>
2020-05-08 22:26:19 +0800

    feat: drag

--- src/components/DynamicFormDesigner/index.tsx
+++ src/components/DynamicFormDesigner/index.tsx
@@ -11,6 +11,15 @@ import {
 } from "react-icons/ai";
 import {DynamicForm} from "../DynamicForm";
 
+interface DraggingInfo {
+    mode: 'move' | 'copy' | null;
+    target: any | null;
+    offsetX: number | null;
+    offsetY: number | null;
+    x: number | null;
+    y: number | null;
+}
+
 export const DynamicFormDesigner = function () {
     const [basicWidgets] = useState<Widget[]>([
         {type: 'input', icon: <AiOutlineEdit/>, name: '单行文本', enable: true},
@@ -21,7 +30,6 @@ export const DynamicFormDesigner = function () {
         {type: 'checkbox', icon: <AiOutlineCheckSquare/>, name: '多选', enable: true},
         {type: 'checkbox', icon: <AiOutlineCheckSquare/>, name: '单选', enable: true},
     ]);
-
     const [advancedWidgets] = useState<Widget[]>([
         {type: 'grid', icon: <AiOutlineTable/>, name: '布局', enable: true},
         {type: 'list', icon: <AiOutlineOrderedList/>, name: '列表', enable: true},
@@ -29,38 +37,81 @@ export const DynamicFormDesigner = function () {
         {type: 'tab', icon: <AiOutlineCopy/>, name: '标签页', enable: false},
     ]);
 
+    const [draggingInfo, setDraggingInfo] = useState<DraggingInfo>({
+        mode: null, offsetX: null, offsetY: null, target: null, x: null, y: null
+    });
+
     return <>
-        <table className={'layout'}>
+        <span>{
+            draggingInfo.x + ' ' + draggingInfo.y
+        }</span>
+        <table className={'layout'} onMouseMove={event => {
+            event.persist();
+            if (draggingInfo.target) {
+                setDraggingInfo(prevState => {
+                    return {
+                        ...prevState,
+                        // @ts-ignore
+                        x: event.nativeEvent.clientX - draggingInfo.offsetX,
+                        // @ts-ignore
+                        y: event.nativeEvent.clientY - draggingInfo.offsetY
+                    }
+                })
+            }
+        }}>
             <tbody>
             <tr>
                 <td className={'left'} rowSpan={2}>
                     <div>基础字段</div>
                     <ul className={'panel'}>
                         {
-                            basicWidgets.map(widget => <li key={widget.name} className={'widget'}>
-                                {widget.icon}
-                                <span>{widget.name}</span>
-                            </li>)
+                            basicWidgets.map(widget => {
+                                return <li key={widget.name} className={'widget'}
+                                           onMouseDown={event => {
+                                               setDraggingInfo({
+                                                   offsetX: event.nativeEvent.offsetX,
+                                                   offsetY: event.nativeEvent.offsetY,
+                                                   target: widget,
+                                                   x: event.nativeEvent.clientX - event.nativeEvent.offsetX,
+                                                   y: event.nativeEvent.clientY - event.nativeEvent.offsetY,
+                                                   mode: 'copy'
+                                               });
+                                           }}>
+                                    {widget.icon}
+                                    <span>{widget.name}</span>
+                                </li>;
+                            })
                         }
                     </ul>
                     <div>高级字段</div>
                     <ul className={'panel'}>
                         {
-                            advancedWidgets.map(widget => <li key={widget.name}
-                                                              className={'widget'}>
-                                {widget.icon}
-                                <span>{widget.name}</span>
-                            </li>)
+                            advancedWidgets.map(widget => {
+                                return <li key={widget.name} className={'widget'}>
+                                    {widget.icon}
+                                    <span>{widget.name}</span>
+                                </li>;
+                            })
                         }
                     </ul>
-                    <div id={"draggable"}/>
+                    <div id={"draggable"}
+                         style={{left: draggingInfo?.x + 'px', top: draggingInfo?.y + 'px'}}>
+                        {
+                            draggingInfo && draggingInfo.target !== null && <>
+                              <li key={draggingInfo.target.name} className={'widget'}>
+                                  {draggingInfo.target.icon}
+                                <span>{draggingInfo.target.name}</span>
+                              </li>
+                            </>
+                        }
+                    </div>
                 </td>
                 <td className={'toolbar'}>
                     <button type={'button'} className={'btn'}>
                         <AiOutlineDelete/>
                         <span>
-                            清空
-                        </span>
+            清空
+            </span>
                     </button>
                     <button type={'button'} className={'btn'}>
                         <AiOutlineEye/>
@@ -92,16 +143,6 @@ export const DynamicFormDesigner = function () {
                                         placeholder: 'test ph',
                                         warningable: true
                                     },
-                                    {
-                                        type: 'input',
-                                        id: '3',
-                                        value: null,
-                                        labeled: true,
-                                        label: 'test',
-                                        required: true,
-                                        placeholder: 'test ph',
-                                        warningable: true
-                                    }
                                 ]
                             }]
                         }}/>
