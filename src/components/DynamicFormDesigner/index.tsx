144ef03d416fc3f7b6243003d2fe3de02c071019
izsl <iamppz@hotmail.com>
2020-05-10 08:22:03 +0800

    refactor: layout

--- src/components/DynamicFormDesigner/index.tsx
+++ src/components/DynamicFormDesigner/index.tsx
@@ -1,18 +1,20 @@
-import './index.css';
 import React, {useState} from 'react';
-import {Widget} from "./schemas";
-import {Element} from "../../schemas/Element";
-
 import {
     AiOutlineCalendar,
-    AiOutlineCheckSquare, AiOutlineCopy, AiOutlineDelete,
+    AiOutlineCheckSquare,
+    AiOutlineCopy,
     AiOutlineDownSquare,
-    AiOutlineEdit, AiOutlineEye, AiOutlineMinus,
-    AiOutlineOrderedList, AiOutlineSave,
+    AiOutlineEdit,
+    AiOutlineMinus,
+    AiOutlineOrderedList,
     AiOutlineTable
 } from "react-icons/ai";
-import {DynamicForm} from "../DynamicForm";
 import {createWidgetInstance, findListOfIndicatorInside} from "./util";
+import {Element} from "../../schemas/Element";
+import {DynamicForm} from "../DynamicForm";
+import {Widget} from "./schemas";
+import './index.css';
+import {Layout, Button, Space} from 'antd';
 
 interface DraggingInfo {
     mode: 'move' | 'copy' | null;
@@ -26,21 +28,31 @@ interface DraggingInfo {
 export const DynamicFormDesignerContext = React.createContext<any>(null);
 
 export const DynamicFormDesigner = function () {
-    const [basicWidgets] = useState<Widget[]>([
-        {type: 'input', icon: <AiOutlineEdit/>, name: '单行文本', enable: true},
-        {type: 'textarea', icon: <AiOutlineEdit/>, name: '多行文本', enable: true},
-        {type: 'dropdown', icon: <AiOutlineDownSquare/>, name: '下拉选择', enable: true},
-        {type: 'datetime', icon: <AiOutlineCalendar/>, name: '日期时间', enable: true},
-        {type: 'tree', icon: <AiOutlineDownSquare/>, name: '级联选择', enable: false},
-        {type: 'checkbox', icon: <AiOutlineCheckSquare/>, name: '多选', enable: true},
-        {type: 'checkbox', icon: <AiOutlineCheckSquare/>, name: '单选', enable: true},
-    ]);
-    const [advancedWidgets] = useState<Widget[]>([
-        {type: 'grid', icon: <AiOutlineTable/>, name: '布局', enable: true},
-        {type: 'list', icon: <AiOutlineOrderedList/>, name: '列表', enable: true},
-        {type: 'separator', icon: <AiOutlineMinus/>, name: '分割线', enable: false},
-        {type: 'tab', icon: <AiOutlineCopy/>, name: '标签页', enable: false},
-    ]);
+    const {Sider, Content, Header} = Layout;
+
+    const widgetGroup: { name: string, widgets: Widget[] }[] = [
+        {
+            name: '基础字段',
+            widgets: [
+                {type: 'input', icon: <AiOutlineEdit/>, name: '单行文本', enable: true},
+                {type: 'textarea', icon: <AiOutlineEdit/>, name: '多行文本', enable: true},
+                {type: 'dropdown', icon: <AiOutlineDownSquare/>, name: '下拉选择', enable: true},
+                {type: 'datetime', icon: <AiOutlineCalendar/>, name: '日期时间', enable: true},
+                {type: 'tree', icon: <AiOutlineDownSquare/>, name: '级联选择', enable: false},
+                {type: 'checkbox', icon: <AiOutlineCheckSquare/>, name: '多选', enable: true},
+                {type: 'checkbox', icon: <AiOutlineCheckSquare/>, name: '单选', enable: true},
+            ]
+        },
+        {
+            name: '高级字段',
+            widgets: [
+                {type: 'grid', icon: <AiOutlineTable/>, name: '布局', enable: true},
+                {type: 'list', icon: <AiOutlineOrderedList/>, name: '列表', enable: true},
+                {type: 'separator', icon: <AiOutlineMinus/>, name: '分割线', enable: false},
+                {type: 'tab', icon: <AiOutlineCopy/>, name: '标签页', enable: false},
+            ]
+        }
+    ];
 
     const [draggingInfo, setDraggingInfo] = useState<DraggingInfo>({
         mode: null, offsetX: null, offsetY: null, target: null, x: null, y: null
@@ -48,26 +60,11 @@ export const DynamicFormDesigner = function () {
 
     const [data, setData] = useState<Element>({
         type: 'grid',
-        id: '1',
-        swimlanes: [{
-            span: 100,
-            elements: [
-                {
-                    type: 'input',
-                    id: '2',
-                    value: null,
-                    labeled: true,
-                    label: 'test',
-                    required: true,
-                    placeholder: 'test ph',
-                    warningable: true
-                },
-            ]
-        }]
+        id: '11270307',
+        swimlanes: [{span: 100, elements: []}]
     });
 
     function handleWidgetMouseDown(event: React.MouseEvent<HTMLLIElement>, widget: Widget) {
-        console.log('down');
         setDraggingInfo({
             offsetX: event.nativeEvent.offsetX,
             offsetY: event.nativeEvent.offsetY,
@@ -86,7 +83,7 @@ export const DynamicFormDesigner = function () {
         });
     }
 
-    function handleLayoutMouseMove(event: React.MouseEvent<HTMLTableElement>) {
+    function handleLayoutMouseMove(event: React.MouseEvent) {
         event.persist();
         if (draggingInfo.target) {
             setDraggingInfo(prevState => {
@@ -101,7 +98,7 @@ export const DynamicFormDesigner = function () {
         }
     }
 
-    function handleLayoutMouseUp(event: React.MouseEvent<HTMLTableElement>) {
+    function handleLayoutMouseUp() {
         const elements = findListOfIndicatorInside(data);
         if (elements != null) {
             const indicator = elements.filter(element => element.type === 'indicator')[0];
@@ -112,72 +109,55 @@ export const DynamicFormDesigner = function () {
         }
     }
 
+    const indicatorPositionStyle = {left: draggingInfo?.x + 'px', top: draggingInfo?.y + 'px'};
     return <>
         <DynamicFormDesignerContext.Provider value={draggingInfo}>
-            <table className={'layout'} onMouseMove={event => handleLayoutMouseMove(event)}
-                   onMouseUp={event => handleLayoutMouseUp(event)}>
-                <tbody>
-                <tr>
-                    <td className={'left'} rowSpan={2}>
-                        <div>基础字段</div>
-                        <ul className={'panel'}>
-                            {
-                                basicWidgets.map(widget =>
-                                    <li key={widget.name} className={'widget'}
-                                        onMouseDown={event => handleWidgetMouseDown(event, widget)}>
-                                        {widget.icon}
-                                        <span>{widget.name}</span>
-                                    </li>)
-                            }
-                        </ul>
-                        <div>高级字段</div>
-                        <ul className={'panel'}>
-                            {
-                                advancedWidgets.map(widget => {
-                                    return <li key={widget.name} className={'widget'}>
-                                        {widget.icon}
-                                        <span>{widget.name}</span>
-                                    </li>;
-                                })
-                            }
-                        </ul>
-                        <div id={"draggable"}
-                             style={{left: draggingInfo?.x + 'px', top: draggingInfo?.y + 'px'}}>
-                            {
-                                draggingInfo && draggingInfo.target !== null && <>
-                                  <li key={draggingInfo.target.name} className={'widget'}>
-                                      {draggingInfo.target.icon}
-                                    <span>{draggingInfo.target.name}</span>
-                                  </li>
-                                </>
-                            }
-                        </div>
-                    </td>
-                    <td className={'toolbar'}>
-                        <button type={'button'} className={'btn'}>
-                            <AiOutlineDelete/>
-                            <span>清空</span>
-                        </button>
-                        <button type={'button'} className={'btn'}>
-                            <AiOutlineEye/>
-                            <span>预览</span>
-                        </button>
-                        <button type={'button'} className={'btn'}>
-                            <AiOutlineSave/>
-                            <span>保存</span>
-                        </button>
-                    </td>
-                    <td className={'right'} rowSpan={2}/>
-                </tr>
-                <tr>
-                    <td className={'form'}>
-                        <div style={{height: '100%', overflowY: 'auto'}}>
+            <Layout className={'layout'}
+                    onMouseMove={event => handleLayoutMouseMove(event)}
+                    onMouseUp={() => handleLayoutMouseUp()}>
+                <Sider width={280} className={'left'}>
+                    {
+                        widgetGroup.map(group => <>
+                            <div style={{color: "white"}}>{group.name}</div>
+                            <ul className={'panel'}>
+                                {
+                                    group.widgets.map((widget: Widget) =>
+                                        <li key={widget.name} className={'widget'}
+                                            onMouseDown={e => handleWidgetMouseDown(e, widget)}>
+                                            {widget.icon}
+                                            <span>{widget.name}</span>
+                                        </li>)
+                                }
+                            </ul>
+                        </>)
+                    }
+                    <div id={"draggable"} style={indicatorPositionStyle}>
+                        {
+                            draggingInfo && draggingInfo.target !== null && <>
+                              <li className={'widget'}>
+                                  {draggingInfo.target.icon}
+                                <span>{draggingInfo.target.name}</span>
+                              </li>
+                            </>
+                        }
+                    </div>
+                </Sider>
+                <Content>
+                    <Layout>
+                        <Header>
+                            <Space>
+                                <Button>清空</Button>
+                                <Button>预览</Button>
+                                <Button>保存</Button>
+                            </Space>
+                        </Header>
+                        <Content>
                             <DynamicForm element={data}/>
-                        </div>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
+                        </Content>
+                    </Layout>
+                </Content>
+                <Sider width={280} className={'right'}/>
+            </Layout>
         </DynamicFormDesignerContext.Provider>
     </>;
 }
