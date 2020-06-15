1d5d18d14d7d117e0bffe93c17307f04bf41060f
izsl <iamppz@hotmail.com>
2020-06-15 22:24:58 +0800

    build: add build/

---
+++ build/precache-manifest.2df5d179c8767a09c8801c3768b37063.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91fce6534bf02a2539351996d03340d3",
+    "url": "/dynamic-form-react/index.html"
+  },
+  {
+    "revision": "f2e933705aa11cb47c75",
+    "url": "/dynamic-form-react/static/css/2.35fcc229.chunk.css"
+  },
+  {
+    "revision": "7ee4ea4c0d7cd7ec671d",
+    "url": "/dynamic-form-react/static/css/main.fe790c40.chunk.css"
+  },
+  {
+    "revision": "f2e933705aa11cb47c75",
+    "url": "/dynamic-form-react/static/js/2.8ea38b09.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/dynamic-form-react/static/js/2.8ea38b09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ee4ea4c0d7cd7ec671d",
+    "url": "/dynamic-form-react/static/js/main.976f9663.chunk.js"
+  },
+  {
+    "revision": "362eb3cdfdb8d6ac32f6",
+    "url": "/dynamic-form-react/static/js/runtime-main.eb2c22b3.js"
+  }
+]);
